e82011f5f2376a611f5a54294e8e7e1999ce4afb
Student Details <moyejemima@gmail.com>
2024-10-09 16:02:00 +0300

    Muli

--- Main .cpp
+++ Main .cpp
@@ -23,3 +23,28 @@ vector<vector<string>> readTeamsFromFile(const string& filename) {/*In simpler t
             getline(s, team[0], ',');
             getline(s, team[1], ',');
             getline(s, team[2], ',');
+            teams.push_back(team);/* The push_back() function adds the team data to the end of the teams vector.
+                          Vectors in C++ are dynamic arrays that can resize themselves automatically.
+                        Reference: Bjarne Stroustrup, The C++ Programming Language, 4th Edition, p. 714. */
+        }
+        myfile.close(); // closes the file once reading is done
+    } else {
+        cerr << "Could not open the file" << "\n";
+    }
+    return teams;
+}
+
+// Function to group teams by their towns
+map<string, vector<string>> groupTeamsByTown(const vector<vector<string>>& teams) {
+    /*map - an associative container that stores elements in key value pairs. key is an identifier used to access a specific value
+     associative container is a type of data structure that stores elements in a way that allows for fast retrieval based on keys/
+    map<string, vector<string>> towns;
+    for (const auto& team : teams) {
+        towns[team[1]].push_back(team[0]); /* team is the current team object from the loop.
+        const auto& declares a reference to each team object in the teams vector. auto means the compiler automatically deduces that
+        each element in the teams vector is of type team
+        push_back adds the team object to the vector of teams from the same town
+        For each team in the vector, add the team to the corresponding town in the map*/
+}
+return towns;
+}
